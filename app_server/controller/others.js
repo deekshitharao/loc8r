d13f56deaca81226ee6f9d1948e899d63c3efaa4
deekshitharao <21eg107a43@anurag.edu.in>
2023-08-19 13:52:50 +0530

    frontend

--- app_server/controller/others.js
+++ app_server/controller/others.js
@@ -4,7 +4,7 @@ module.exports.about= function(req,res){
     res.render('generic-text',{title:'About'});
 };
 module.exports.signin= function(req,res){
-    res.render('sign-in');
+    res.render('signin',{title:'SignIN'});
 };
 module.exports.register=function(req,res){
     res.render('register-page');
